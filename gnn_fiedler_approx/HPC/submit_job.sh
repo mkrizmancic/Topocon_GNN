63593f21987af3470a972d55d5a61899f33576c1
Marko Krizmancic <marko.krizmancic@fer.hr>
2025-01-22 13:59:08 +0100

    [l2_approx] Run script with wandb as job on HPC

---
+++ gnn_fiedler_approx/HPC/submit_job.sh
@@ -0,0 +1,12 @@
+#PBS -N GNN_l2_approx
+#PBS -q login-gpu
+#PBS -o /lustre/home/mkrizman/Topocon_GNN/gnn_fiedler_approx/HPC/jobs/output/
+#PBS -e /lustre/home/mkrizman/Topocon_GNN/gnn_fiedler_approx/HPC/jobs/error/
+#PBS -l select=1:ncpus=4:ngpus=1:mem=16GB
+
+export http_proxy="http://10.150.1.1:3128"
+export https_proxy="http://10.150.1.1:3128"
+
+cd $HOME/Topocon_GNN/gnn_fiedler_approx
+
+apptainer run --nv HPC/gnn_fiedler.sif gnn_fiedler_approx/algebraic_connectivity_script.py --standalone --eval-type basic --eval-target best
